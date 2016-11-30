e6110949e213d00b584c1f3f7cd0e86d620f4b4a
alexandre.punch <alexandre.punch@alterway.fr>
2016-11-30 17:28:31 +0100

    Refactoring setup.sh

--- scripts/scaleway-docker/setup.sh
+++ scripts/scaleway-docker/setup.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
+
 # This script will ensure ansible is installed on the system,
-# configure /etc/ansible/hosts if needed, ans launch the
-# playbooks.
+# configure /etc/ansible/hosts if needed, ans launch the playbooks.
 
 # Get current work directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Check if user-config and ansible hosts are present
-if [ ! -f $DIR/user-config.cfg ]; then
+if [ ! -f $DIR/user-config.cfg ]; then 
     echo "user-config file not found!"
     exit 1
-fi
-if [ ! -f $DIR/ansible/hosts ]; then
+elif [ ! -f $DIR/ansible/hosts ]; then
     echo "Ansible hosts file not found!"
     exit 1
 fi
 
 # Read config file
-echo "Reading config...." >&2
 source $DIR/user-config.cfg
 
 # Install ansible dependencies
-apt-get install gcc g++ python-dev libffi-dev libssl-dev -y
+apt-get install -qqy gcc g++ python-dev libffi-dev libssl-dev
 
 # Install/upgrade pip and ansible
-wget https://bootstrap.pypa.io/get-pip.py -P /tmp
-python /tmp/get-pip.py
-pip install --upgrade pip
-pip install ansible
-pip install --upgrade ansible
+if command -v pip >/dev/null 2>&1; then
+    if [ -f /tmp/get-pip.py ]; then rm /tmp/get-pip.py; fi;
+    wget https://bootstrap.pypa.io/get-pip.py -P /tmp
+    python /tmp/get-pip.py
+else
+    pip install --upgrade pip
+elif command -v ansible >/dev/null 2>&1
+    pip install ansible
+else
+    pip install --upgrade ansible
+fi
 
 # Read ansible hosts and accept rsa keys
 ssh-keyscan -f "$DIR/ansible/hosts" >> ~/.ssh/known_hosts

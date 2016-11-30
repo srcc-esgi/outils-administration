dcbc4f76b7008d0c9f30f4698d49445644ffe800
alexandre.punch <alexandre.punch@alterway.fr>
2016-11-30 19:15:22 +0100

    Refactoring setup.sh - 2

--- scripts/scaleway-docker/setup.sh
+++ scripts/scaleway-docker/setup.sh
@@ -21,21 +21,33 @@ source $DIR/user-config.cfg
 # Install ansible dependencies
 apt-get install -qqy gcc g++ python-dev libffi-dev libssl-dev
 
-# Install/upgrade pip and ansible
+# Install/upgrade pip
 if command -v pip >/dev/null 2>&1; then
     if [ -f /tmp/get-pip.py ]; then rm /tmp/get-pip.py; fi;
     wget https://bootstrap.pypa.io/get-pip.py -P /tmp
     python /tmp/get-pip.py
 else
-    pip install --upgrade pip
-elif command -v ansible >/dev/null 2>&1
-    pip install ansible
-else
-    pip install --upgrade ansible
+    pip install --upgrade --quiet pip
 fi
 
-# Read ansible hosts and accept rsa keys
-ssh-keyscan -f "$DIR/ansible/hosts" >> ~/.ssh/known_hosts
+# Install/upgrade ansible
+if command -v ansible >/dev/null 2>&1; then
+    pip install ansible
+else
+    pip install --upgrade --quiet ansible
+fi
+
+# Read ansible hosts and accept rsa key
+ansible_hosts=$(grep -v '^#' "$DIR/ansible/hosts")
+for host in $ansible_hosts; do
+    host_keyscan=$(ssh-keyscan -H $host)
+    if grep -Fxq $host $DIR/ansible/hosts
+        echo $host_keyscan >> /home/apunch/.ssh/known_hosts
+        then echo "Host $host added to known_hosts."
+    else
+       echo "Host $host is known, skiping."
+    fi
+done
 
 # Launch playbook
-ansible-playbook $DIR/ansible/deploy.yml -i $DIR/ansible/hosts
+su -c "ansible-playbook $DIR/ansible/deploy.yml -i $DIR/ansible/hosts" $SUDO_USER
